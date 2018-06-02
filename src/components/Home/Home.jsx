2197e032ab5cf6c1e4367b93570295a33d6c4eb1
Alfred <ChimerezeAlfred@Chimerezes-MacBook-Pro.local>
2018-06-02 23:30:41 +0100

    updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -130,72 +130,100 @@ export default class Home extends Component {
                     </div>
                 </div>
                 <div className="testimonial-area">
-                <div className="area-header"> 
-                <h2 className="area-header-h2">Testimonial from our Clients</h2>
-                <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from</p>
+                    <div className="area-header">
+                        <h2 className="area-header-h2">Testimonial from our Clients</h2>
+                        <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from</p>
+                    </div>
+                    <Carousel>
+                        <Carousel.Item>
+                            <div className="card">
+                                <div className="card1">
+                                    <div className="card-container">
+                                        <img src={require('./image/testtimonial-1.jpg')} width="80px" height="80px" alt="card" />
+                                        <div className="card-media">
+                                            <p>As conscious traveling Paupers we must always be
+                           <br />
+                                                concerned about our dear Mother Earth. If you think
+                            <br />
+                                                about it, you travel across her face, and She is the </p>
+                                            <h4 className="card-h4">Fanny Spencer</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card2">
+                                    <div className="card-container">
+                                        <img src={require('./image/testtimonial-1.jpg')} width="80px" height="80px" alt="card" />
+                                        <div className="card-media">
+                                            <p>As conscious traveling Paupers we must always be
+                           <br />
+                                                concerned about our dear Mother Earth. If you think
+                            <br />
+                                                about it, you travel across her face, and She is the </p>
+                                            <h4 className="card-h4">Fanny Spencer</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className="card">
+                                <div className="card1">
+                                    <div className="card-container">
+                                        <img src={require('./image/testtimonial-1.jpg')} width="80px" height="80px" alt="card" />
+                                        <div className="card-media">
+                                            <p>As conscious traveling Paupers we must always be
+                           <br />
+                                                concerned about our dear Mother Earth. If you think
+                            <br />
+                                                about it, you travel across her face, and She is the </p>
+                                            <h4 className="card-h4">Fanny Spencer</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card2">
+                                    <div className="card-container">
+                                        <img src={require('./image/testtimonial-1.jpg')} width="80px" height="80px" alt="card" />
+                                        <div className="card-media">
+                                            <p>As conscious traveling Paupers we must always be
+                           <br />
+                                                concerned about our dear Mother Earth. If you think
+                            <br />
+                                                about it, you travel across her face, and She is the </p>
+                                            <h4 className="card-h4">Fanny Spencer</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>;
                 </div>
-                <Carousel>
-                    <Carousel.Item>
-                        <div className="card">
-                            <div className="card1">
-                                <div className="card-container">
-                                    <img src={require('./image/testtimonial-1.jpg')} width="60px" height="60px" alt="card" />
-                                    <div className="card-media">
-                                        <p>As conscious traveling Paupers we must always be
-                           <br />
-                                            concerned about our dear Mother Earth. If you think
-                            <br />
-                                            about it, you travel across her face, and She is the </p>
-                                        <h4 className="card-h4">Fanny Spencer</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card2">
-                                <div className="card-container">
-                                    <img src={require('./image/testtimonial-1.jpg')} width="60px" height="60px" alt="card" />
-                                    <div className="card-media">
-                                        <p>As conscious traveling Paupers we must always be
-                           <br />
-                                            concerned about our dear Mother Earth. If you think
-                            <br />
-                                            about it, you travel across her face, and She is the </p>
-                                        <h4 className="card-h4">Fanny Spencer</h4>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="post-blog">
+                    <div className="post-head">
+                        <h2 className="post-head-h2">
+                            latest posts from blog
+                </h2>
+                        <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from</p>
+                    </div>
+                    <div className="blog-post">
+                    <div className="blog1">
+                        <div className="blog-image1">
+                        <img src={require('./image/blog-1.jpg')} alt="blog" />
+                            <div></div>
                         </div>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <div className="card">
-                            <div className="card1">
-                                <div className="card-container">
-                                    <img src={require('./image/testtimonial-1.jpg')} width="60px" height="60px" alt="card" />
-                                    <div className="card-media">
-                                        <p>As conscious traveling Paupers we must always be
-                           <br />
-                                            concerned about our dear Mother Earth. If you think
-                            <br />
-                                            about it, you travel across her face, and She is the </p>
-                                        <h4 className="card-h4">Fanny Spencer</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card2">
-                                <div className="card-container">
-                                    <img src={require('./image/testtimonial-1.jpg')} width="60px" height="60px" alt="card" />
-                                    <div className="card-media">
-                                        <p>As conscious traveling Paupers we must always be
-                           <br />
-                                            concerned about our dear Mother Earth. If you think
-                            <br />
-                                            about it, you travel across her face, and She is the </p>
-                                        <h4 className="card-h4">Fanny Spencer</h4>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
-                    </Carousel.Item>
-                </Carousel>;
+                        <div className="blog2">
+                        <div className="blog-image2">
+                        <img src={require('./image/blog-2.jpg')} alt="blog" />
+                            <div></div>
+                        </div>
+                        </div>
+                        <div className="blog3">
+                        <div className="blog-image3">
+                        <img src={require('./image/blog-3.jpg')} alt="blog" />
+                            <div></div>
+                        </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
